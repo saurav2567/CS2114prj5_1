7866b308f54c1300c3829396c228361559a5a995
andrewmedberry <andrewm05@vt.edu>
2026-04-20 15:13:41 -0400

    deleted name classes

--- src/prj5/InputFileReader.java
+++ src/prj5/InputFileReader.java
@@ -1,4 +1,5 @@
 package prj5;
+import java.util.*;
 // -------------------------------------------------------------------------
 /**
  *  Write a one-sentence summary of your class here.
@@ -8,12 +9,30 @@ package prj5;
  *  @author Andrew Medberry
  *  @version Apr 20, 2026
  */
+import student.IOHelper;
 public class InputFileReader
 {
-    //~ Fields ................................................................
+    Scanner inStream = IOHelper.createScanner("");
+    inStream.nextLine();// skip header
+    while (inStream.hasNextLine()) {
 
-    //~ Constructors ..........................................................
 
-    //~Public  Methods ........................................................
+        String line = inStream.nextLine().replaceAll(" ", "");
+        String[] values = line.split(",");
+        String month = values[0];
+        String username = values[1];
+        String channel = values[2];
+        String country = values[3];
+        String mainTopic = values[4];
+        int likes = toInt(values[5]);
+        int posts = toInt(values[6]);
+        int followers = toInt(values[7]);
+        int comments = toInt(values[8]);
+        int views = toInt(values[9]);
 
+
+        // TODO : Populate the Classes created to store the data
+
+
+    } // end while
 }
