1077923fd3a41010b5e84797b05c534316bb8992
andrewmedberry <andrewm05@vt.edu>
2026-04-20 15:07:03 -0400

    Created the file reader
    not working just yet because I need to get the import statement for the scanner.

---
+++ src/prj5/InputFileReader.java
@@ -0,0 +1,19 @@
+package prj5;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Andrew Medberry
+ *  @version Apr 20, 2026
+ */
+public class InputFileReader
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
