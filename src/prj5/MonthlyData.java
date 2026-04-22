f65500283ac573898ee2aa9bfad444f5ad2be98a
andrewmedberry <andrewm05@vt.edu>
2026-04-21 23:38:46 -0400

    Finished InputFileReader and the test class for MonthlyData
    some things might not work as the dependencies dont exist yet.

--- src/prj5/MonthlyData.java
+++ src/prj5/MonthlyData.java
@@ -1,3 +1,17 @@
+// Virginia Tech Honor Code Pledge:
+
+//
+
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+
+// -- Andrew Medberry (906594739)
+// LLM Statement:
+
+// I have not used any assistance for the assignment beyond course resources and
+// staff.
 package prj5;
 
 // -------------------------------------------------------------------------
