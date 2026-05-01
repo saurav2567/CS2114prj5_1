f9f50e5a45bc67de28823fbecbdf1d1f7688662d
andrewmedberry <andrewm05@vt.edu>
2026-05-01 18:15:50 -0400

    created GUI
    :/

---
+++ src/prj5/GUI.java
@@ -0,0 +1,25 @@
+package prj5;
+
+import cs2.*;
+
+// -------------------------------------------------------------------------
+/**
+ *  The graphical user interface for the project
+ * 
+ *  @author Andrew Medberry
+ *  @version Apr 30, 2026
+ */
+public class GUI
+{
+    private Window window;
+    private Button quitButton;
+    private Button sortByChannelButton;
+    private Button sortByEngagementButton;
+    private Button traditionalEngagementButton;
+    private Button reachEngagementButton;
+    private Button firstQuarterButton;
+    private Button januaryButton;
+    private Button februaryButton;
+    private Button marchButton;
+    
+}
